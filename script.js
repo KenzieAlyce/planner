0145a679328a1be6699de7d42005c9c9d9299d52
KenzieAlyce <73435380+KenzieAlyce@users.noreply.github.com>
2021-01-13 14:26:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,30 +5,27 @@ currentDay.html(moment().format("dddd, MMMM Do"));
 const currentHour = moment().hour();
 
 function colorBlocks() {
-  var rowBlocks = $(".row");
-  var timeBlock;
+  var rowBlocks = $(".color");
+ 
   for (var i = 0; i < rowBlocks.length; i++) {
-    timeBlock = rowBlocks[i];
+    var timeBlock = rowBlocks[i];
 
-    if (timeBlock.dataset.time < currentHour) { 
+    if (timeBlock.dataset.time < currentHour) {  
+      $(rowBlocks).addClass ("past")
         //do something for the past
+
     } else if (timeBlock.dataset.time > currentHour) {
+      $(rowBlocks).addClass ("future")
         // do something for the future
 
     } else {
+
+      $(rowBlocks).addClass ("present")
         //do something for present 
     }
 
   }
 }
-
-
-
-// local storage 24 hr
-// JSON  stringify
-
-// if today > than stored day? clear storage & refresh?
-
-// buttons
-
-//For Loop*/
+console.log (rowBlocks)
+  console.log (timeBlock)
+  console.log (currentHour)
