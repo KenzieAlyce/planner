19a3dfdf657b5b73a727937524d62dce667abfc5
KenzieAlyce <73435380+KenzieAlyce@users.noreply.github.com>
2021-01-14 21:36:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,31 +7,23 @@ currentDay.html(moment().format("dddd, MMMM Do"));
 const currentHour = moment().hour();
 
   var rowBlocks = $(".color"); 
+
+   var button1 = $("#button1");
+   var textBox1 = $('#textBox1');
+   var theText = $('textBox1.val');
+  var theTextArray = [""];
+
+
+
+  $("#button1").on('click', saveLocal());
   
-colorBlocks ();
+   function saveLocal() {
+localStorage.setItem(textBox1.val, 
+JSON.stringify.theTextArray) 
 
-function colorBlocks() {
- 
-  for (var i = 0; i < rowBlocks.length; i++) {
- 
-   var timeBlock = rowBlocks[i];
+//then 
 
-    if (timeBlock.dataset.time < currentHour) {  
-      $(timeBlock).addClass ("past")
-        //do something for the past
+//if (theText = null)
+//JSON.parse(localStorage.getItem("theTextArray")
 
-    } else if (timeBlock.dataset.time > currentHour) {
-      $(timeBlock).addClass ("future")
-        // do something for the future
-
-    } else {
-
-      $(timeBlock).addClass ("present")
-        //do something for present 
-    }
-
-  }
 }
-
-}); 
-
