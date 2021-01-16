69cd54896faa397ae7b699679bb873ffa1cb6797
KenzieAlyce <73435380+KenzieAlyce@users.noreply.github.com>
2021-01-15 20:58:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
 var schedulerArray = localStorage.getItem("scheduling") !== null ? JSON.parse(localStorage.getItem("scheduling")) : [];
-
-
 const currentHour = moment().hour();
 var rowBlocks = $(".color");
 colorBlocks ();
-
 function colorBlocks() {
- 
   for (var i = 0; i < rowBlocks.length; i++) {
- 
    var timeBlock = rowBlocks[i];
-
     if (timeBlock.dataset.time < currentHour) {  
       $(timeBlock).addClass ("past")
         //do something for the past
-
     } else if (timeBlock.dataset.time > currentHour) {
       $(timeBlock).addClass ("future")
-        // do something for the future
-
     } else {
-
       $(timeBlock).addClass ("present")
-        //do something for present 
     }
-
   }
 }
 var saveScheduler = function(){
-
 var tempArry = []
 $("#scheduling .row").each(function(){
     var rowTime = $(this).data("time")
@@ -38,15 +25,9 @@ $("#scheduling .row").each(function(){
         [rowTime]: rowTaskText
     })
 })
-
  localStorage.setItem("scheduling",JSON.stringify(tempArry));
-
-
-
 }
-
-
-// local scheduler   get item
+// local scheduler 
 var loadScheduler = function(){
  if (schedulerArray.length === 0 ){return}
  $.each(schedulerArray, function(index, objData){
@@ -57,19 +38,5 @@ var loadScheduler = function(){
  })
 }
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
-// every time user clicked save button
-
-   
-
-// function backgroundUpdate() {
-//      var currentTime = moment().hours() (currentTime)    (".time-block").each(function(){ var id = $(this).attr("id") 
-
-//       if (id < currentTime) { $(this).addClass("past") } else if(id === currentTime) 
-//       {  $(this).addClass("present") }
-//        else  { $(this).addClass("future")}  
-
-// })}
-
 $(".saveBtn").on("click",saveScheduler);
- 
  loadScheduler();
