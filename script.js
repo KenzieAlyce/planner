e8f54cb62e63f68067f176bb5e5be223c52b3e93
KenzieAlyce <73435380+KenzieAlyce@users.noreply.github.com>
2021-01-15 21:37:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,58 @@
-var schedulerArray = localStorage.getItem("scheduling") !== null ? JSON.parse(localStorage.getItem("scheduling")) : [];
+$(document).ready(function() {
 
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
+var currentDay = $("#currentDay");
+
+currentDay.html(moment().format("dddd, MMMM Do"));
+
 const currentHour = moment().hour();
-var rowBlocks = $(".color");
 
+   var rowBlocks = $(".color"); 
+   var button1 = $("#button1");
+   var textBox1 = $('#textBox1');
+   var theText = $('textBox1.val');
+
+  
 colorBlocks ();
 
 function colorBlocks() {
+ 
   for (var i = 0; i < rowBlocks.length; i++) {
+ 
    var timeBlock = rowBlocks[i];
+
     if (timeBlock.dataset.time < currentHour) {  
       $(timeBlock).addClass ("past")
         //do something for the past
+
     } else if (timeBlock.dataset.time > currentHour) {
       $(timeBlock).addClass ("future")
+        // do something for the future
     } else {
+
       $(timeBlock).addClass ("present")
+        //do something for present 
     }
+
+  }
+}
+  
+populateTextAreas()
+function populateTextAreas() {
+  text_areas = $(".enterText")
+  
+  for (i = 0; i > text_areas.length; i++) {
+      var stored_text = localStorage.getItem(text_areas[i].id)
+      text_areas[i].value = stored_text
   }
 }
 
-var saveScheduler = function(){
-var tempArry = []
-$("#scheduling .row").each(function(){
-    var rowTime = $(this).data("time")
-    var rowTask = $(this).find("textarea") //div:nth-child(2)
-    var rowTaskText = rowTask.val().trim()
-    tempArry.push({
-        [rowTime]: rowTaskText
-    })
-})
+$(".saveBtn").on("click", function(e) {
+  e.preventDefault();
+  
+  var hour_id = this.previousElementSibling.id;
+  var hour_text = this.previousElementSibling.value;
+  
+  localStorage.setItem(hour_id, hour_text);
+}); 
+});
 
- localStorage.setItem("scheduling",JSON.stringify(tempArry));
-}
-// local scheduler 
-
-var loadScheduler = function(){
- if (schedulerArray.length === 0 ){return}
- $.each(schedulerArray, function(index, objData){
-     var hour = Object.keys(objData)[0];
-     var task = Object.values(objData)[0];
-     var targetRow = $("#scheduling").find(`[data-time=${hour}]`);
-     targetRow.find("textarea").text(task);
- })
-}
-$(".saveBtn").on("click",saveScheduler);
- loadScheduler();
