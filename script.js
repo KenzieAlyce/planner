f9d8e33016f8bfa23e9d4590b2c2381108841994
KenzieAlyce <73435380+KenzieAlyce@users.noreply.github.com>
2021-01-15 20:55:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-$(document).ready(function() {
+var schedulerArray = localStorage.getItem("scheduling") !== null ? JSON.parse(localStorage.getItem("scheduling")) : [];
 
-var currentDay = $("#currentDay");
-
-currentDay.html(moment().format("dddd, MMMM Do"));
 
 const currentHour = moment().hour();
-
-   var rowBlocks = $(".color"); 
-   var button1 = $("#button1");
-   var textBox1 = $('#textBox1');
-   var theText = $('textBox1.val');
-   var theTextArray = [""];
-  
+var rowBlocks = $(".color");
 colorBlocks ();
 
 function colorBlocks() {
@@ -22,34 +13,63 @@ function colorBlocks() {
 
     if (timeBlock.dataset.time < currentHour) {  
       $(timeBlock).addClass ("past")
+        //do something for the past
 
     } else if (timeBlock.dataset.time > currentHour) {
       $(timeBlock).addClass ("future")
+        // do something for the future
+
     } else {
 
       $(timeBlock).addClass ("present")
+        //do something for present 
     }
 
   }
 }
-  
+var saveScheduler = function(){
 
-//function addText() {
-  //text_areas = $(".text-area")
-  
-  //for (i = 0; i > text_areas.length; i++) {
-    //  var stored_text = localStorage.getItem(text_areas[i].id)
-      //text_areas[i].value = stored_text
-  //}
-//}
+var tempArry = []
+$("#scheduling .row").each(function(){
+    var rowTime = $(this).data("time")
+    var rowTask = $(this).find("textarea") //div:nth-child(2)
+    var rowTaskText = rowTask.val().trim()
+    tempArry.push({
+        [rowTime]: rowTaskText
+    })
+})
 
-//$(".saveBtn").on("click", function(e) {
-  //e.preventDefault();
-  
-  //var hour_id = this.previousElementSibling.id;
-  //var hour_text = this.previousElementSibling.value;
-  
- // localStorage.setItem(hour_id, hour_text);
-//}); 
-});
+ localStorage.setItem("scheduling",JSON.stringify(tempArry));
 
+
+
+}
+
+
+// local scheduler   get item
+var loadScheduler = function(){
+ if (schedulerArray.length === 0 ){return}
+ $.each(schedulerArray, function(index, objData){
+     var hour = Object.keys(objData)[0];
+     var task = Object.values(objData)[0];
+     var targetRow = $("#scheduling").find(`[data-time=${hour}]`);
+     targetRow.find("textarea").text(task);
+ })
+}
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+// every time user clicked save button
+
+   
+
+// function backgroundUpdate() {
+//      var currentTime = moment().hours() (currentTime)    (".time-block").each(function(){ var id = $(this).attr("id") 
+
+//       if (id < currentTime) { $(this).addClass("past") } else if(id === currentTime) 
+//       {  $(this).addClass("present") }
+//        else  { $(this).addClass("future")}  
+
+// })}
+
+$(".saveBtn").on("click",saveScheduler);
+ 
+ loadScheduler();
